890de27f3aac6e0767fa68e6c6cb200f3a874a26
Martin Lasek <martinlasek@hotmail.de>
2017-10-08 23:02:25 +0200

    added footer styles

---
+++ Frontend/src/api/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import store from '../store/index';
+import router from '../router/index';
+
+const BASE_URL = 'http://127.0.0.1:8020';
+
+//pragma mark - use for PRODUCTION
+//const BASE_URL = '';
--- Frontend/src/components/layout/Footer.vue
+++ Frontend/src/components/layout/Footer.vue
@@ -3,3 +3,7 @@
     <a href="https://github.com/vapor/vapor">Built with Vapor</a>
   </footer>
 </template>
+
+<style lang="less">
+  @import "../../assets/styles/min/Footer.less";
+</style>
