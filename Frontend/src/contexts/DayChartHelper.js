93efd1a532d7f8f2b5fe749fece354e0e8e98541
Grazyna Lasek <martinlasek@hotmail.de>
2018-01-20 22:24:54 +0100

    added .idea and Package.resolved to gitignore

---
+++ Frontend/src/contexts/DayChartHelper.js
@@ -0,0 +1,23 @@
+import Helper from '../utils/Helper';
+import DateHelper from '../utils/DateHelper';
+import LineChartHelper from '../utils/LineChartHelper';
+import {frameworks} from '../utils/constants'
+
+export default class DayChartHelper {
+
+  static createDataSetForEachRepo(days, repoList) {
+
+    return repoList.map(repo => {
+
+      const processedStars = LineChartHelper.createListOfStarsForEachDay(days, repo.stars);
+      const dataSet = {
+        label: repo.name,
+        data: processedStars.map(star => star.amount),
+        chartLabels: processedStars.map(star => DateHelper.getDayFromDate(star.created_at)),
+        originData: processedStars
+      };
+
+      return {...dataSet, ...frameworks[repo.name].lineChartStyle};
+    });
+  }
+}
