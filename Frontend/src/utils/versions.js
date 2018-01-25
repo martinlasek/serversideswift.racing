9e5e9569152e360e826a21340b3021c864d547ee
Grazyna Lasek <martinlasek@hotmail.de>
2018-01-25 21:48:00 +0100

    fix github-corner thanks to @twof 🤜 🤛

--- Frontend/src/utils/versions.js
+++ Frontend/src/utils/versions.js
@@ -7,7 +7,7 @@ export const versions = [
     changes: [
       'redesign of whole website',
       'chart displays last 30 days from today on',
-      'stars are crawled every hour',
+      'stars are scraped every hour',
       'growth is calculated based on last 365 days',
       'frontend is separated from backend (api)',
       'framework links to its respective github repo',
